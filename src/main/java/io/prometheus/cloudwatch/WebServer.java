fa6b992ee688ec9214a9541c521d94f6cd890c78
Daniel Bonatto <bonatto@gmail.com>
2017-11-30 15:12:53 -0200

    Improving catastrophic errors logs.

--- src/main/java/io/prometheus/cloudwatch/WebServer.java
+++ src/main/java/io/prometheus/cloudwatch/WebServer.java
@@ -2,26 +2,44 @@ package io.prometheus.cloudwatch;
 
 import io.prometheus.client.exporter.MetricsServlet;
 import java.io.FileReader;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
 
+
 public class WebServer {
-   public static void main(String[] args) throws Exception {
+
+  private static final Logger LOGGER = Logger.getLogger(CollectorWorker.class.getName());
+
+  public static void main(String[] args) throws Throwable {
      if (args.length < 2) {
        System.err.println("Usage: WebServer <port> <yml configuration file>");
        System.exit(1);
      }
      CloudWatchCollector cc = new CloudWatchCollector(new FileReader(args[1])).register();
 
-     int port = Integer.parseInt(args[0]);
-     Server server = new Server(port);
-     ServletContextHandler context = new ServletContextHandler();
-     context.setContextPath("/");
-     server.setHandler(context);
-     context.addServlet(new ServletHolder(new MetricsServlet()), "/metrics");
-     context.addServlet(new ServletHolder(new HomePageServlet()), "/");
-     server.start();
-     server.join();
+     Thread.setDefaultUncaughtExceptionHandler(new Thread.UncaughtExceptionHandler() {
+       public void uncaughtException(Thread t, Throwable e) {
+         System.err.println(t + " throws exception: " + e);
+       }
+     });
+
+     try {
+       int port = Integer.parseInt(args[0]);
+       Server server = new Server(port);
+       ServletContextHandler context = new ServletContextHandler();
+       context.setContextPath("/");
+       server.setHandler(context);
+       context.addServlet(new ServletHolder(new MetricsServlet()), "/metrics");
+       context.addServlet(new ServletHolder(new HomePageServlet()), "/");
+       server.start();
+       server.join();
+     } catch (Throwable e) {
+        LOGGER.log(Level.SEVERE, "Error while running the webserver.", e);
+        throw e;
+     }
    }
 }
